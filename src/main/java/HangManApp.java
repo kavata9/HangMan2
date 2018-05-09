a30abbb9d2700d8d2efc33c8a7ef118a95aaa7cf
Agnes Kavata <kavatamusyoka24@gmail.com>
2018-05-09 03:31:05 +0300

    add test to select a random word from arrays

--- src/main/java/HangManApp.java
+++ src/main/java/HangManApp.java
@@ -1,9 +1,23 @@
+import java.util.Random;
+import java.util.Arrays;
+import java.util.Scanner;
+import java.io.Console;
+// This checks that the function should return a string data
 public class HangManApp {
   private String guessedWord;
   private String myInput;
-  //This Shows that it returns a String
-    public String words(){
-      return "Test";
-    }
-    
+
+  public String words() {
+    return "Gradle";
   }
+
+  // This Functions will Show the random word
+  public String randWords() {
+    Random myRandom = new Random();
+    String[] myWords = { "Gradle", "HangMan", "Build", "Peace" };
+    int myIndex = myRandom.nextInt(1);
+
+    guessedWord = myWords[myIndex];
+    return guessedWord;
+  }
+}
--- src/test/java/HangManTest.java
+++ src/test/java/HangManTest.java
@@ -1,14 +1,16 @@
 import org.junit.Test;
 import static org.junit.Assert.*;
+import java.util.Arrays;
+
 public class HangManTest {
 @Test
   public void getStringWord() {
     HangManApp word = new HangManApp();
     assertEquals("Test", word.words());
   }
+  // This will Test if the random picker works
   @Test
-  public void getStringWordrandom() {
-    HangManApp word = new HangManApp();
-    assertArrayEquals("Project", word.randomword);
-  }
+  public void getRandomword() {
+    HangManApp words = new HangManApp();
+    assertEquals("Gradle", words.randWords());
 }
